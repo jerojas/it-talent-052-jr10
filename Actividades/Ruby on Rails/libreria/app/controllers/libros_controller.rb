14ff8f7b4bca5b2ef4247616b2277fa2b4690cf0
Jeovany <jerojas@unac.edu.co>
2018-07-04 21:32:20 -0500

    serviciosapi

--- Actividades/Ruby on Rails/libreria/app/controllers/libros_controller.rb
+++ Actividades/Ruby on Rails/libreria/app/controllers/libros_controller.rb
@@ -1,5 +1,5 @@
 class LibrosController < ApplicationController
-
+protect_from_forgery unless: -> {request.format.json?}
 
 def listar
 
@@ -8,4 +8,47 @@ def listar
 
 end 
 
+
+def create
+
+b = eval (request.body.read)
+render json: {
+
+	status: 'ok'
+}
+
+	end
+
+
+def traer_libros
+	end 
+
+
+def get_libros
+
+libros = Libro.all
+response = []
+libros.each do |b|
+response.push({
+
+	id: b.id,
+	titulo: b.nombre,
+	descripcion: b.descripcion,
+
+	autors: {
+		
+		id: b.autor.id,
+		nombre: b.autor.nombre
+	}
+})
+
+end 
+	render json: {
+
+		libros: response
+	}
 end
+
+	end
+
+
--- Actividades/Ruby on Rails/libreria/config/routes.rb
+++ Actividades/Ruby on Rails/libreria/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   get "libros" => "libros#listar"
   get "autores" => "autores#listar"
+
+  get "servicio" => "libros#get_libros"
+  get "consultar" => "libros#traer_libros"
+  post "create" => "libros#create"
+
 end
